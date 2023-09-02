b48d4e52a8abebddcbb06444aed4e45f9e5cac34
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-03 00:59:11 +0600

    token verification added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -11,7 +11,7 @@ router.post('/signup', userController.signupUser);
 router.post('/login', userController.loginUser);
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), userController.getAllUsers);
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.getSingleUser);
-router.patch('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.updateUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.deleteUser);
 
 export const userRouter = router;
