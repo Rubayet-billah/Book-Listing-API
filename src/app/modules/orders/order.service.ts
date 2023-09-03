21cb2f30fd1358672af2be00602cdff8fe9e4fd3
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-04 02:10:21 +0600

    simple user validation added

--- src/app/modules/orders/order.service.ts
+++ src/app/modules/orders/order.service.ts
@@ -1,6 +1,8 @@
 // src/app/modules/order/order.service.ts
 
 import { Order } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
 const createOrder = async (userId: string, orderData: any): Promise<Order> => {
@@ -27,6 +29,16 @@ const getAllOrders = async (): Promise<Order[]> => {
 
 const getCustomerOrders = async (customerId: string): Promise<Order[]> => {
   // Implement your logic to retrieve orders for a specific customer here
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      id: customerId,
+    },
+  });
+
+  if (!existingUser) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
   const customerOrders = await prisma.order.findMany({
     where: {
       userId: customerId,
